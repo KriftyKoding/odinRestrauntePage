5a5e365fe7ecdab7046fdc13674404e26729fd0a
Krifty <kaitlynmariejones@gmail.com>
2022-01-05 11:41:25 -0800

    contact page

---
+++ src/contact.js
@@ -0,0 +1,24 @@
+function contact() {
+    const element = document.getElementById("content");
+    const headline = document.createElement('h1');
+    headline.textContent = "sno restraunte"
+
+    const inputName = document.createElement("INPUT");
+    inputName.setAttribute("type", "text");
+    inputName.setAttribute("value", "Name");
+
+    const inputEmail = document.createElement("INPUT");
+    inputEmail.setAttribute("type", "email");
+    inputEmail.setAttribute("value", "Email");
+
+    const inputText = document.createElement("textarea");
+    inputText.innerText = "Comments...."
+
+    element.appendChild(headline);
+    element.appendChild(inputName);
+    element.appendChild(inputEmail);
+    element.appendChild(inputText);
+    
+}
+
+module.exports =  contact;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ console.log("TestTestTest");
 import _ from 'lodash';
 import homePage from "./homePage";
 import navBar from "./navBar"
+import contact from "./contact"
 
 navBar();
-homePage();
+// homePage();
+contact();
 
